1bc1ab3a0fb804dea9574c0fcc8a92e5053e18c5
kaynnan <kaynnanx1@gmail.com>
2025-02-06 08:50:13 -0300

    [ADD] clog_account_payment_order_custom: add new module

---
+++ clog_account_payment_order_custom/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CLOG - Account Payment Order - Custom",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/clog-addons",
+    "depends": ["account_payment_order"],
+    "data": [
+        "report/account_payment_order.xml",
+        "report/print_account_payment_order.xml",
+    ],
+}
